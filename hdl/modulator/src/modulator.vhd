79b33ab55e767345ea39abdef97e6a677dd66fc7
Stefan Stähli <stefan@fam-staeh.li>
2025-12-15 08:54:17 +0100

    Refactor modulator two process implementation

--- hdl/modulator/src/modulator.vhd
+++ hdl/modulator/src/modulator.vhd
@@ -1,3 +1,16 @@
+---------------------------------------------------------------------------------------------------
+-- Copyright (c) 2025 Stefan Stähli
+---------------------------------------------------------------------------------------------------
+
+---------------------------------------------------------------------------------------------------
+-- Description
+---------------------------------------------------------------------------------------------------
+-- This entity implements a combined Sine and Cosine generator using a lookup table with optional
+-- linear interpolation.
+
+---------------------------------------------------------------------------------------------------
+-- Libraries
+---------------------------------------------------------------------------------------------------
 library IEEE;
   use IEEE.STD_LOGIC_1164.ALL;
   use IEEE.NUMERIC_STD.ALL;
@@ -7,126 +20,133 @@ library olo;
   use olo.en_cl_fix_pkg.all;
   use olo.olo_fix_pkg.all;
 
+---------------------------------------------------------------------------------------------------
+-- Entity Declaration
+---------------------------------------------------------------------------------------------------
 entity Modulator is
     generic (
-           gDataWidth : natural;
-           gLUTWidth : natural);
-    port ( clk : in std_logic;
-           reset_n : in std_logic;
-           iAngle : in std_logic_vector(gDataWidth-1 downto 0); -- fix_fmt(1,0,datawidth-1) / signed
-           oSin : out std_logic_vector(gDataWidth-1 downto 0); -- fix_fmt(1,0,datawidth-1) / signed
-           oCos : out std_logic_vector(gDataWidth-1 downto 0)); -- fix_fmt(1,0,datawidth-1) / signed
+        DataWidth_g  : natural;
+        LutWidth_g   : natural
+    );
+    port (
+        Clk          : in    std_logic;
+        Angle        : in    std_logic_vector(DataWidth_g-1 downto 0); -- fix_fmt(1,0,datawidth-1) / signed
+        Sine         : out   std_logic_vector(DataWidth_g-1 downto 0); -- fix_fmt(1,0,datawidth-1) / signed
+        Cosine       : out   std_logic_vector(DataWidth_g-1 downto 0) -- fix_fmt(1,0,datawidth-1) / signed
+    );
 end Modulator;
 
-architecture Behavioral of Modulator is
+---------------------------------------------------------------------------------------------------
+-- Architecture Declaration
+---------------------------------------------------------------------------------------------------
+architecture rtl of Modulator is
     
     -- since the fix format we use is signed, the table is half the size of the value range
     -- sin(-a) = sin(360-a)
-    constant cFixFmt : FixFormat_t := (1, 0, gDataWidth-1);
-    constant cSinTableWidth : integer := minimum(gLUTWidth, gDataWidth-1);
-    constant cSinTableSize : natural := 2**(cSinTableWidth);
-    constant c90Deg : natural := (2**(gDataWidth-1)-1)/4;
-    constant cRemainderFmt : FixFormat_t := (0,0,gDataWidth-1-cSinTableWidth);
+    constant SinTableWidth_c    : integer       := minimum(LutWidth_g, DataWidth_g-1);
+    constant SinTableSize_c     : natural       := 2**(SinTableWidth_c);
+    constant FixFmt_c           : FixFormat_t   := (1, 0, DataWidth_g-1);
+    constant RemainderFormat_c  : FixFormat_t   := (0, 0, DataWidth_g-1-SinTableWidth_c);
+    constant A90Deg_c           : natural       := (2**(DataWidth_g-1)-1)/4;
 
     -- define discrete sSine values of one whole period (= table size)
-	type tSinLut is array (0 to cSinTableSize-1) of std_logic_vector(cl_fix_width(cFixFmt)-1 downto 0);
-	signal sSinTable : tSinLut;
+	type SinLut_t is array (0 to SinTableSize_c-1) of std_logic_vector(cl_fix_width(FixFmt_c)-1 downto 0);
+	signal SinTable : SinLut_t;
     
-    -- intermediate signals
-    signal sAngle, sAngle_f : std_logic_vector(cl_fix_width(cFixFmt)-1 downto 0) := (others => '0');
-    signal sSine, sCosine : std_logic_vector(cl_fix_width(cFixFmt)-1 downto 0) := (others => '0');
-    signal sSineLUT, sSineLUT_f, sSineDelta, sSineDelta_f : unsigned(cl_fix_width(cFixFmt)-1 downto 0) := (others => '0');
-    signal sCOsineLUT, sCOsineLUT_f, sCosineDelta, sCosineDelta_f : unsigned(cl_fix_width(cFixFmt)-1 downto 0) := (others => '0');
+    type TwoProcess_r is record
+        Angle           : std_logic_vector(DataWidth_g-1 downto 0);
+        SineLutVal      : unsigned(cl_fix_width(FixFmt_c)-1 downto 0);
+        SineLinearSeg   : unsigned(cl_fix_width(FixFmt_c)-1 downto 0);
+        CosineLutVal    : unsigned(cl_fix_width(FixFmt_c)-1 downto 0);
+        CosineLinearSeg : unsigned(cl_fix_width(FixFmt_c)-1 downto 0);
+        Sine            : unsigned(cl_fix_width(FixFmt_c)-1 downto 0);
+        Cosine          : unsigned(cl_fix_width(FixFmt_c)-1 downto 0);
+    end record;
+
+    signal r, r_next : TwoProcess_r;
 
 begin
   	
--- create array with sSine values for sin
--- cos will be determined with sin(a) --> cos(a) = sin(90-a)
-table : for i in 0 to cSinTableSize-1 generate
-    sSinTable(i) <= cl_fix_from_real(sin(2.0*MATH_PI*real(i)/real(cSinTableSize)),cFixFmt);
-end generate table;
+    -----------------------------------------------------------------------------------------------
+    -- LUT
+    -----------------------------------------------------------------------------------------------
+    -- create array with sSine values for sin
+    -- cos will be determined with sin(a) --> cos(a) = sin(90-a)
+    table : for i in 0 to SinTableSize_c-1 generate
+        SinTable(i) <= cl_fix_from_real(sin(2.0*MATH_PI*real(i)/real(SinTableSize_c)),FixFmt_c);
+    end generate table;
 
--- two process model (Jiri Gaisler)
-p_sequential: process(clk)
-begin
-    if rising_edge(clk) then
-        sAngle <= iAngle;
-        oSin <= sSine;
-        oCos <= sCosine;
-        -- pipeline stage 1
-        sAngle_f <= sAngle;
-        sSineLUT_f <= sSineLUT;
-        sCosineLUT_f <= sCosineLUT;
-        sSineDelta_f <= sSineDelta;
-        sCosineDelta_f <= sCosineDelta;
-        -- synchroneous reset
-        if reset_n = '0' then
-            sAngle <= (others => '0');
-            oSin <= (others => '0');
-            oCos <= (others => '0');
-            sAngle_f <= (others => '0');
-            sSineLUT_f <= (others => '0');
-            sCosineLUT_f <= (others => '0');
-            sSineDelta_f <= (others => '0');
-            sCosineDelta_f <= (others => '0');
-        end if;       
-    end if;
-end process p_sequential;
+    -----------------------------------------------------------------------------------------------
+    -- Combinatorial Proccess
+    -----------------------------------------------------------------------------------------------
+    p_combinatorial: process(all) is
+        variable v : TwoProcess_r;
+        
+        -- lut table indizes, make use of the wrapping with vector size
+        variable vSinFullIndex, vCosFullIndex : unsigned(DataWidth_g-2 downto 0);
+        alias aSinTableIndex : unsigned(SinTableWidth_c-1 downto 0)
+            is vSinFullIndex(DataWidth_g-2 downto DataWidth_g-1-SinTableWidth_c);
+        alias aCosTableIndex : unsigned(SinTableWidth_c-1 downto 0)
+            is vCosFullIndex(DataWidth_g-2 downto DataWidth_g-1-SinTableWidth_c);
+        
+        -- interpolation
+        alias aRemainder : std_logic_vector(DataWidth_g-2-SinTableWidth_c downto 0)
+            is Angle(DataWidth_g-2-SinTableWidth_c downto 0);
 
-p_combinatorial: process(all)
-    
-    -- lut table indizes, make use of the wrapping with vector size
-    variable vSinFullIndex, vCosFullIndex : unsigned(gDataWidth-2 downto 0);
-    alias aSinTableIndex : unsigned(cSinTableWidth-1 downto 0)
-        is vSinFullIndex(gDataWidth-2 downto gDataWidth-1-cSinTableWidth);
-    alias aCosTableIndex : unsigned(cSinTableWidth-1 downto 0)
-        is vCosFullIndex(gDataWidth-2 downto gDataWidth-1-cSinTableWidth);
-    
-    -- interpolation
-    alias aRemainder : std_logic_vector(gDataWidth-2-cSinTableWidth downto 0)
-        is sAngle_f(gDataWidth-2-cSinTableWidth downto 0);
+    begin
+        -- *** hold variables stable ***
+        v := r;
 
-    variable vSinDelta, vCosDelta : unsigned(cl_fix_width(cFixFmt)-1 downto 0) := (others => '0');
+        -- *** Default Values ***
+        -- sin index is directly coupled to the angle
+        vSinFullIndex := unsigned(Angle(vSinFullIndex'range));
+        
+        -- cos index is 90-angle
+        vCosFullIndex := to_unsigned(A90Deg_c,vCosFullIndex'length) - unsigned(Angle(vCosFullIndex'range));
 
-begin
+        -- lookup sin/cos in table
+        v.SineLutVal := unsigned(SinTable(to_integer(aSinTableIndex)));
+        v.CosineLutVal := unsigned(SinTable(to_integer(aCosTableIndex+1)));
 
-    -- pipeline stage 1: LUT
-    -- sin index is directly coupled to the angle
-    vSinFullIndex := unsigned(sAngle(vSinFullIndex'range));
-    
-    -- cos index is 90-angle
-    vCosFullIndex := to_unsigned(c90Deg,vCosFullIndex'length) - unsigned(sAngle(vCosFullIndex'range));
+        -- delta is always 0 if table is same width as angle input
+        v.SineLinearSeg := (others => '0');
+        v.CosineLinearSeg := (others => '0');
 
-    -- lookup sin/cos in table
-    sSineLUT <= unsigned(sSinTable(to_integer(aSinTableIndex)));
-    sCosineLUT <= unsigned(sSinTable(to_integer(aCosTableIndex+1)));
+        -- otherwise calc remainder for sin/cos interpolation        
+        if ((DataWidth_g-1) > SinTableWidth_c) then
+            -- pipeline stage 1: calculate delta
+            -- take next index, subract current index and interpolate
+            v.SineLinearSeg := unsigned(SinTable(to_integer(aSinTableIndex+1))) - unsigned(SinTable(to_integer(aSinTableIndex)));
+            -- cos table pointer rotataes opposite to sSine (90° -> - <- alpha)
+            v.CosineLinearSeg := unsigned(SinTable(to_integer(aCosTableIndex))) - unsigned(SinTable(to_integer(aCosTableIndex+1)));    
+        end if;
+            
+        v.Sine := r.SineLutVal + unsigned(cl_fix_mult(
+                std_logic_vector(r.SineLinearSeg), FixFmt_c,
+                aRemainder, RemainderFormat_c,
+                FixFmt_c));
+        v.Cosine := r.CosineLutVal + unsigned(cl_fix_mult(
+                std_logic_vector(r.CosineLinearSeg), FixFmt_c,
+                aRemainder, RemainderFormat_c,
+                FixFmt_c));
 
-    -- delta is always 0 if table is same width as angle input
-    sSineDelta <= (others => '0');
-    sCosineDelta <= (others => '0');
-    vSinDelta := (others => '0');
-    vCosDelta := (others => '0');
+        r_next <= v;
+    end process p_combinatorial;
 
-    -- otherwise calc remainder for sin/cos interpolation        
-    if ((gDataWidth-1) > cSinTableWidth) then
-    
-        -- pipeline stage 1: calculate delta
-        -- take next index, subract current index and interpolate
-        sSineDelta <= unsigned(sSinTable(to_integer(aSinTableIndex+1))) - unsigned(sSinTable(to_integer(aSinTableIndex)));
-        -- cos table pointer rotataes opposite to sSine (90° -> - <- alpha)
-        sCosineDelta <= unsigned(sSinTable(to_integer(aCosTableIndex))) - unsigned(sSinTable(to_integer(aCosTableIndex+1)));
-    
-        -- pipeline stage 2: multiply
-        -- multiply with remainder
-        vSinDelta := unsigned(cl_fix_mult(std_logic_vector(sSineDelta_f),cFixFmt,aRemainder,cRemainderFmt,cFixFmt));
-        vCosDelta := unsigned(cl_fix_mult(std_logic_vector(sCosineDelta_f),cFixFmt,aRemainder,cRemainderFmt,cFixFmt));
+    -----------------------------------------------------------------------------------------------
+    -- Outputs
+    -----------------------------------------------------------------------------------------------
+    Sine   <= std_logic_vector(r.Sine);
+    Cosine <= std_logic_vector(r.Cosine);
 
-    end if;
-    
-    -- pipeline stage 2: Add LUT and Interpolation
-    sSine <= std_logic_vector(sSineLUT_f + vSinDelta);
-    sCosine <= std_logic_vector(sCosineLUT_f + vCosDelta);
+    -----------------------------------------------------------------------------------------------
+    -- Sequential Proccess
+    -----------------------------------------------------------------------------------------------
+    p_seq : process (Clk) is
+    begin
+        if rising_edge(Clk) then
+            r <= r_next;
+        end if;
+    end process;
 
-end process p_combinatorial;
-
-end Behavioral;
+end architecture;
--- hdl/modulator/test/modulator_tb.vhd
+++ hdl/modulator/test/modulator_tb.vhd
@@ -20,12 +20,11 @@ architecture tb of modulator_tb is
 
   constant cFixFmt : FixFormat_t := (1,0,11);
 
-  constant cTestLimitTable : real := 0.01;
-  constant cTestLimitInterpol : real := 0.05;
+  constant TestLimitTable_c : real := 0.01;
+  constant TestLimitInterpol_c : real := 0.05;
 
-  signal clk : std_logic := '0';
-  signal reset_n : std_logic := '0';
-  signal angle : std_logic_vector(cl_fix_width(cFixFmt)-1 downto 0) := (others => '0');
+  signal Clk : std_logic := '0';
+  signal Angle : std_logic_vector(cl_fix_width(cFixFmt)-1 downto 0) := (others => '0');
   signal sine_table, sine_interpol, cosine_table, cosine_interpol : std_logic_vector(cl_fix_width(cFixFmt)-1 downto 0);
   signal sRealSinRef, sRealCosRef, sRealSinTable, sRealCosTable, sRealSinInterpol, sRealCosInterpol : real;
 
@@ -35,46 +34,26 @@ begin
   begin
     test_runner_setup(runner, runner_cfg);
     
-    -- test comparator
-    if run("modulator_test_out_of_reset") then
+    if run("modulator_test_values") then
 
-      angle <= cl_fix_from_real(0.0,cFixFmt);
-      wait until rising_edge(clk);
-      wait for 100 ps;
-      check_equal(sine_table, cl_fix_from_real(0.0,cFixFmt), result("zero"));
-      check_equal(cosine_table, cl_fix_from_real(0.0,cFixFmt), result("zero"));
-      check_equal(sine_interpol, cl_fix_from_real(0.0,cFixFmt), result("zero"));
-      check_equal(cosine_interpol, cl_fix_from_real(0.0,cFixFmt), result("zero"));
-      wait until reset_n = '1';
-      wait until rising_edge(clk);
-      wait until rising_edge(clk);
-      wait for 100 ps;
-      check_equal(sine_table, cl_fix_from_real(0.0,cFixFmt), result("still zero"));
-      check_equal(cosine_table, cl_fix_from_real(1.0,cFixFmt), result("one"));
-      check_equal(sine_interpol, cl_fix_from_real(0.0,cFixFmt), result("still zero"));
-      check_equal(cosine_interpol, cl_fix_from_real(1.0,cFixFmt), result("one"));
-
-    elsif run("modulator_test_values") then
-
-      wait until reset_n = '1';
-      for i in (-(2**angle'high)) to (2**angle'high)-1 loop
-        wait until falling_edge(clk);
-        angle <= cl_fix_from_real(real(i)/real((2**angle'high)-1),cFixFmt);
-        wait until rising_edge(clk);
+      for i in (-(2**Angle'high)) to (2**Angle'high)-1 loop
+        wait until falling_edge(Clk);
+        Angle <= cl_fix_from_real(real(i)/real((2**Angle'high)-1),cFixFmt);
+        wait until rising_edge(Clk);
         -- wait for pipeline to settle
-        wait until rising_edge(clk);
-        wait until rising_edge(clk);
-        sRealSinRef <= sin(2.0*MATH_PI*real(i)/real((2**angle'high)-1));
-        sRealCosRef <= cos(2.0*MATH_PI*real(i)/real((2**angle'high)-1));
+        wait until rising_edge(Clk);
+        wait until rising_edge(Clk);
+        sRealSinRef <= sin(2.0*MATH_PI*real(i)/real((2**Angle'high)-1));
+        sRealCosRef <= cos(2.0*MATH_PI*real(i)/real((2**Angle'high)-1));
         sRealSinTable <= cl_fix_to_real(sine_table,cFixFmt);
         sRealSinInterpol <= cl_fix_to_real(sine_interpol,cFixFmt);
         sRealCosTable <= cl_fix_to_real(cosine_table,cFixFmt);
         sRealCosInterpol <= cl_fix_to_real(cosine_interpol,cFixFmt);
         wait for 100 ps;
-        check_equal(sRealSinTable, sRealSinRef, max_diff => cTestLimitTable);
-        check_equal(sRealSinInterpol, sRealSinRef, max_diff => cTestLimitInterpol);
-        check_equal(sRealCosTable, sRealCosRef, max_diff => cTestLimitTable);
-        check_equal(sRealCosInterpol, sRealCosRef, max_diff => cTestLimitInterpol);
+        check_equal(sRealSinTable, sRealSinRef, max_diff => TestLimitTable_c);
+        check_equal(sRealSinInterpol, sRealSinRef, max_diff => TestLimitInterpol_c);
+        check_equal(sRealCosTable, sRealCosRef, max_diff => TestLimitTable_c);
+        check_equal(sRealCosInterpol, sRealCosRef, max_diff => TestLimitInterpol_c);
       end loop;
       
     end if;
@@ -83,35 +62,31 @@ begin
   end process;
 
   -- generate clock
-  clk <= not clk after 10 ns;
+  Clk <= not Clk after 10 ns;
 
-  -- deassert reset
-  reset_n <= '1' after 77 ns;
 
   dut1: entity project.Modulator
     generic map (
-      gDataWidth => 12,
-      gLUTWidth => 12
+      DataWidth_g => 12,
+      LUTWidth_g => 12
     )
     port map (
-      clk => clk,
-      reset_n => reset_n,
-      iAngle => angle,
-      oSin => sine_table,
-      oCos => cosine_table
+      Clk => Clk,
+      Angle => Angle,
+      Sine => sine_table,
+      Cosine => cosine_table
     );
 
     dut2: entity project.Modulator
       generic map (
-        gDataWidth => 12,
-        gLUTWidth => 4
+        DataWidth_g => 12,
+        LUTWidth_g => 4
       )
       port map (
-        clk => clk,
-        reset_n => reset_n,
-        iAngle => angle,
-        oSin => sine_interpol,
-        oCos => cosine_interpol
+        Clk => Clk,
+        Angle => Angle,
+        Sine => sine_interpol,
+        Cosine => cosine_interpol
       );
 
 end architecture;
